47eef3b0c49f6e92caf8c8c36a1a89f0447ec195
cristian-ungureanu <cristian@themeisle.com>
2016-12-29 16:02:21 +0200

    #102 - add upsells for the plus plugin

--- inc/customize-info/class/class-azera-shop-customize-upsell.php
+++ inc/customize-info/class/class-azera-shop-customize-upsell.php
@@ -85,7 +85,7 @@ final class Azera_Shop_Customizer_Upsell {
 
 		if(!class_exists('Azera_Shop_Plus')){
 			$manager->add_section( new Azera_Shop_Customizer_Upsell_Frontpage_Sections( $manager, 'azera-shop-upsell', array(
-				'upsell_text'               => sprintf( esc_html__( 'Check out the %sPRO version%s for full control over the frontpage SECTIONS ORDER!.', 'azera-shop' ), '<a href="' . esc_url( 'https://themeisle.com/plugins/azera-shop-plus/' ) . '">', '</a>' ),
+				'upsell_text'               => sprintf( esc_html__( 'Check out the %sPRO version%s for full control over the frontpage SECTIONS ORDER!.', 'azera-shop' ), '<a href="' . esc_url( 'https://themeisle.com/plugins/azera-shop-plus/' ) . '" target="_blank">', '</a>' ),
 				'panel'                     => 'azera_shop_front_page_sections',
 				'priority'                  => 500,
 			) ) );
