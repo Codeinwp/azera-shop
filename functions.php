eafecb744c5c1b53b57754693ce04082119cadea
stefanc <stefan@themeisle.com>
2017-09-14 16:57:57 +0300

    Sidebar added for shop page

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ if ( ! function_exists( 'azera_shop_setup' ) ) :
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
 		 * hard-coded <title> tag in the document head, and expect WordPress to
-         * provide it for us.
+		 * provide it for us.
 		 */
 		add_theme_support( 'title-tag' );
 
@@ -228,6 +228,19 @@ function azera_shop_widgets_init() {
 		)
 	);
 
+	if ( class_exists( 'WooCommerce' ) ) {
+		register_sidebar(
+			array(
+				'name'          => esc_html__( 'WooCommerce Sidebar', 'azera-shop' ),
+				'id'            => 'sidebar-woocommerce',
+				'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</aside>',
+				'before_title'  => apply_filters( 'azera_shop_widgets_before_title','<h2 class="widget-title">' ),
+				'after_title'   => apply_filters( 'azera_shop_widgets_after_title','</h2><div class="colored-line-left"></div><div class="clearfix widget-title-margin"></div>' ),
+			)
+		);
+	}
+
 	register_sidebars(
 		4,
 		array(
@@ -375,8 +388,8 @@ add_action( 'wp_head', 'azera_shop_ie' );
 
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
-add_action( 'woocommerce_before_main_content', 'azera_shop_wrapper_start', 10 );
-add_action( 'woocommerce_after_main_content', 'azera_shop_wrapper_end', 10 );
+add_action( 'woocommerce_before_main_content', 'azera_shop_wrapper_start_trigger_shop_page', 10 );
+add_action( 'woocommerce_after_main_content', 'azera_shop_wrapper_end_shop_page', 10 );
 
 /**
  * Wrapper start for content.
@@ -404,7 +417,7 @@ function azera_shop_wrapper_start( $class = 'col-md-12', $is_blog = false ) {
 }
 
 /**
- * Wraooer end for content.
+ * Wrapper end for content.
  *
  * @param bool $has_sidebar     if is sidebar then get sidebar.
  */
@@ -414,6 +427,38 @@ function azera_shop_wrapper_end( $has_sidebar = false ) {
 	<?php
 	if ( $has_sidebar == true ) {
 		get_sidebar();
+	}
+	?>
+	</div>
+	</div>
+	<?php
+}
+
+/**
+ * Set wrapper width to col-md-8 instead of 12 if sidebar is active
+ *
+ * @since 1.1.11
+ */
+function azera_shop_wrapper_start_trigger_shop_page() {
+
+	if ( is_active_sidebar( 'sidebar-woocommerce' ) ) {
+		azera_shop_wrapper_start( 'col-md-8', false );
+	} else {
+		azera_shop_wrapper_start( 'col-md-12', false );
+	}
+}
+
+/**
+ * Display shop sidebar if is active
+ *
+ * @since 1.1.11
+ */
+function azera_shop_wrapper_end_shop_page() {
+	?>
+	</div>
+	<?php
+	if ( is_active_sidebar( 'sidebar-woocommerce' ) ) {
+		get_sidebar( 'shop' );
 	}
 	?>
 	</div>
